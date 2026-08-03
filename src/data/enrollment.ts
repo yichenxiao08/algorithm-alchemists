f66c218bf89168f3502232b24f31e7333e83cf4d
Ethan Guan <158070820+RandomPerson5571@users.noreply.github.com>
2026-08-03 10:03:46 -0400

    updated dates

--- src/data/enrollment.ts
+++ src/data/enrollment.ts
@@ -1,9 +1,9 @@
 // Single place to update enrollment urgency.
 export const enrollment = {
   // TODO: confirm close date with organizers
-  enrollmentClosesAt: "2026-08-02T23:59:59-04:00",
+  enrollmentClosesAt: "2026-08-09T23:59:59-04:00",
   cohortStartLabel: "July 26",
-  enrollmentClosesLabel: "August 2",
+  enrollmentClosesLabel: "August 9",
   // TODO: confirm capacity
   maxStudentsPerClass: 25,
 };
